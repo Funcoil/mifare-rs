e5cc4a5a7a9f6b8323c5cc81e498cf8046cc1a5a
Martin Habovštiak <martin.habovstiak@gmail.com>
2016-12-03 12:26:20 +0100

    Added write methods. Updated some doc.

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 Pure Rust implementation of Mifare protocol
 ===========================================
 
-This crate implements basic Mifare operations - Authentication and Reading. It's designed to be reader-agnostic so in order to use it, you just need to `impl NFCTag for YourType`. However, it already has impl for PN532 reader (enabled via feature `with_pn532`).
+Warning
+-------
 
-It's still WIP. Use at your own risk!
+This crate is work in progress and may permanently damage your tag. Also, the interface WILL change. Use with care (especially write methods), at your own risk!
+
+About
+-----
+
+This crate implements basic Mifare operations - Authentication and Reading. It's designed to be reader-agnostic so in order to use it, you just need to `impl NFCTag for YourType`. However, it already has impl for PN532 reader (enabled via feature `with_pn532`).
--- src/lib.rs
+++ src/lib.rs
@@ -71,6 +71,9 @@ pub struct AuthenticatedSector<'a, T: 'a> {
 }
 
 impl<'a, T: 'a + NFCTag> AuthenticatedSector<'a, T> {
+    /// Reads 16 bytes of data from given block
+    ///
+    /// Warning: This interface is temporary and will change!
     pub fn read_block(&mut self, pos: u8, buf: &mut [u8]) -> Result<(), T::TransceiveError> {
         if pos > 3 {
             panic!("Position can't be bigger than 3! (Was {}.)", pos);
@@ -79,5 +82,37 @@ impl<'a, T: 'a + NFCTag> AuthenticatedSector<'a, T> {
         let read_cmd = [0x30, self.sector_number * 4 + pos];
         try!(self.tag.tag.transceive(&read_cmd, buf));
         Ok(())
+    }
+
+    fn write_block_raw(&mut self, pos: u8, data: &[u8; 16]) -> Result<(), T::TransceiveError> {
+        let mut write_cmd = [0; 18];
+        write_cmd[0] = 0xA0;
+        write_cmd[1] = self.sector_number * 4 + pos;
+        write_cmd[2..].copy_from_slice(&*data);
+
+        let mut resp = [0; 16];
+        try!(self.tag.tag.transceive(&write_cmd, &mut resp));
+        Ok(())
+    }
+
+    /// Writes 16 bytes of data to given block
+    ///
+    /// WARNING: NOT tested!!! Use at your own risk! By writing incorrect values, you may
+    /// permanently damage the tag!
+    /// This interface is temporary and will change!
+    pub fn write_block(&mut self, pos: u8, data: &[u8; 16]) -> Result<(), T::TransceiveError> {
+        if pos > 2 {
+            panic!("writing to invalid block ({}; valid range is 0...2)", pos);
+        }
+        self.write_block_raw(pos, data)
+    }
+
+    /// Writes keys as well as access bits
+    ///
+    /// WARNING: NOT tested!!! Use at your own risk! By writing incorrect values, you may
+    /// permanently damage the tag!
+    /// This interface is temporary and will change!
+    pub fn write_keys(&mut self, data: &[u8; 16]) -> Result<(), T::TransceiveError> {
+        self.write_block_raw(3, data)
     }
 }
